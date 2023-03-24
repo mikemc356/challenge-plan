4d1c0680251bc8a2384e1801eff0838699b67043
mikemc356 <mikemc@live.co.uk>
2023-03-24 13:54:15 +0000

    Update receive.py

--- receive.py
+++ receive.py
@@ -1,5 +1,33 @@
 #!/usr/bin/env python
 import pika, sys, os
+from minio import Minio
+from minio.error import S3Error
+
+def process_file(name):
+    try:
+        # Create a client with the MinIO server playground, its access key
+        # and secret key.
+        printf(f'Processing file {name}')
+        client = Minio(
+            "http://minio-mike-mcnamee.flows-dev-cluster-7c309b11fc78649918d0c8b91bcb5925-0000.eu-gb.containers.appdomain.cloud",
+            access_key="miniouser",
+            secret_key="miniopassword",
+        )
+
+        # Make 'asiatrip' bucket if not exist.
+        found = client.bucket_exists("input")
+        if not found:
+            print("Bucket 'input' does not exist")
+        else:
+            print("Bucket 'input' already exists")
+    
+        response = client.get_object(
+                bucket_name = 'input',
+                object_name= name
+            )
+
+    except Exception as e:
+        print('Exception {e}')
 
 def main():
     connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
@@ -9,6 +37,8 @@ def main():
 
     def callback(ch, method, properties, body):
         print(" [x] Received %r" % body)
+        print(" [x] Received props %r" % properties)
+        process_file(body.Key)
     
     channel.basic_consume(queue='unpacker-queue',
                       auto_ack=True,
